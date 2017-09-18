0fed589e555af8ab55cf78b4e13615894d3f4b54
harady <harady15@gmail.com>
2017-09-19 00:47:39 +0900

    ここまでは問題なし。

--- CocosCreatorSamples/assets/framework/TestPageScene.ts
+++
@@ -1,6 +0,0 @@
-
-const { ccclass, property } = cc._decorator;
-
-@ccclass
-export default class TestPageScene extends SGFW.PageSceneRoot {
-}
--- CocosCreatorSamples/assets/framework/TitlePageScene.ts
+++
@@ -1,6 +0,0 @@
-
-const { ccclass, property } = cc._decorator;
-
-@ccclass
-export default class NewClass extends SGFW.PageSceneRoot {
-}
